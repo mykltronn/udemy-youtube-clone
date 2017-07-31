18ed7b3ec163f8429ece8e2aa1271c08d9bb04c5
Michael Ashton <mykl.ashton@gmail.com>
2017-07-31 13:50:46 -0400

    Replace existing master with new content

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+const API_KEY = "AIzaSyB2OAcszdbo3PrDa04VMwWSx6DjoPKXQqg"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+          <h1>You're good to go! Now fix the internet...</h1>
+      </div>
+    );
+  }
+}
